8412b655e112a5b4b6aa891520d70a5b5b8d97ac
David Hoese <david.hoese@ssec.wisc.edu>
2019-12-09 01:02:58 -0600

    Add day 9 (needs rewrite)

---
+++ day09/main1.py
@@ -0,0 +1,14 @@
+import sys
+import numpy as np
+from intcode import IntCodeComputer, parse_args
+
+
+def main():
+    args, file_in = parse_args()
+    instructions = np.loadtxt(file_in, delimiter=',', dtype=np.int)
+    comp = IntCodeComputer(stdin=iter([1]))
+    print(list(comp.run(instructions)))
+
+
+if __name__ == "__main__":
+    sys.exit(main())
